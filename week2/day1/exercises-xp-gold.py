108a2433c6bfe6854586b24e19a2ba7b5544aecd
Tzvi Gettenberg <tzvigettenberg@Davids-MacBook-Pro-2.local>
2024-03-11 17:11:50 +0200

    Day 1 exercise xp-gold completed

---
+++ week2/day1/exercises-xp-gold.py
@@ -0,0 +1,14 @@
+# print("I Love Python \n" * 4 + "Hello World! \n" * 4)
+
+# month_selected = int(input("Input a Month, 1 - 12: "))
+
+# if(month_selected > 2 and month_selected < 6):
+#     print("Spring")
+# elif(month_selected > 5 and month_selected < 9):
+#     print("Summer")
+# elif(month_selected > 8 and month_selected < 12):
+#     print("Autumn")
+# elif(month_selected == 12 or month_selected < 3):
+#     print("Winter")
+# else:
+#     print("Invalid Input")
