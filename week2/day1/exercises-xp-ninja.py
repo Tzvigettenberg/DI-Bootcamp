bbaa741221e198c0298922e6de1fd2f325a434ba
Tzvi Gettenberg <tzvigettenberg@Davids-MacBook-Pro-2.local>
2024-03-12 16:42:22 +0200

    ninja completed

---
+++ week2/day1/exercises-xp-ninja.py
@@ -0,0 +1,25 @@
+# my_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+#            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+#            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+#            laboris nisi ut aliquip ex ea commodo consequat. 
+#            Duis aute irure dolor in reprehenderit in voluptate velit 
+#            esse cillum dolore eu fugiat nulla pariatur. 
+#            Excepteur sint occaecat cupidatat non proident, 
+#            sunt in culpa qui officia deserunt mollit anim id est laborum.
+#            '''
+
+# print(len(my_text))
+
+# longest_sentence = 0
+# while True:
+#     user_sentence = input('Input the longest sentence you can without the letter "A"')
+#     user_sentence = user_sentence.lower()
+#     if "a" not in user_sentence and len(user_sentence) > longest_sentence:
+#         longest_sentence = len(user_sentence)
+#         print("Congrats! New Longest Sentence! Score: ", longest_sentence)
+#     else:
+#         print(f'You had an "a" in your sentence. Final Score: {longest_sentence}')
+#         break
+#     if user_sentence == "end":
+#         break
+
